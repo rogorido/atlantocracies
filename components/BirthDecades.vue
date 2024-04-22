669b203ef97dea907c7598e82fb92a54f2bde168
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2024-04-22 18:39:24 +0200

    BirthDecades component

---
+++ components/BirthDecades.vue
@@ -0,0 +1,18 @@
+<template>
+  <Chart type="bar" :data="chartData" :options="chartOptions" />
+</template>
+
+<script setup>
+import Chart from "primevue/chart";
+
+const props = defineProps({
+  chartData: {
+    type: Object,
+    required: true,
+  },
+  chartOptions: {
+    type: Object,
+    default: () => {},
+  },
+});
+</script>
