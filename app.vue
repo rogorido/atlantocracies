9fa6100bfd99e9e8c81f0cf6933e2c07532ba852
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2024-04-22 18:38:43 +0200

    Main changes in config and app.vue

--- app.vue
+++ app.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
-    <v-app>
-      <NuxtWelcome />
-    </v-app>
+    <MenuBar />
+    <NuxtPage />
+    <FooterGeneral />
   </div>
 </template>
+
+<script>
+import "primevue/resources/themes/aura-light-green/theme.css";
+
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+// import "bootstrap/dist/css/bootstrap-grid.css";
+import "@/assets/owncontainers.css";
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
-
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  build: {
-    transpile: ['vuetify'],
+  build: {},
+  ssr: false,
+  modules: ["nuxt-primevue", "@nuxt/content"],
+  $development: {
+    debug: true,
   },
-  modules: [
-    (_options, nuxt) => {
-      nuxt.hooks.hook('vite:extendConfig', (config) => {
-        // @ts-expect-error
-        config.plugins.push(vuetify({ autoImport: true }))
-      })
-    },
-  ],
-  vite: {
-    vue: {
-      template: {
-        transformAssetUrls,
-      },
-    },
-  },
-})
-
+});
