dd3a0477bb9c64dedca8bab35f9264913d3fc0c7
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2024-04-22 23:42:40 +0200

    Chart genders

---
+++ components/GendersChart.vue
@@ -0,0 +1,23 @@
+<template>
+  <Chart
+    type="pie"
+    :data="chartData"
+    :options="chartOptions"
+    class="w-full md:w-30rem"
+  />
+</template>
+
+<script setup>
+import Chart from "primevue/chart";
+
+const props = defineProps({
+  chartData: {
+    type: Object,
+    required: true,
+  },
+  chartOptions: {
+    type: Object,
+    default: () => {},
+  },
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,39 @@
-<template></template>
+<template>
+  <h1>Atlantocracies</h1>
+  <p>Esto es una prueba para ver si funciona...</p>
+  <p>{{ stats }}</p>
+
+  <GendersChart
+    v-if="loaded"
+    :chartData="stats.gendersChartData"
+    :chartOptions="options"
+  />
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const stats = ref();
+
+// Variable to check whether data are loaded or not
+// https://vue-chartjs.org/guide/#chart-with-api-data
+const loaded = ref(false);
+
+const options = { responsive: true };
+
+// important! data, pending etc. coming from useFetch are refs()!!!
+const loadData = async () => {
+  try {
+    const { data } = await useFetch(
+      "http://127.0.0.1:8008/general/generalstats"
+    );
+
+    stats.value = data.value;
+    loaded.value = true;
+  } catch (err) {
+    console.log("we get this error in loadData():", err);
+  }
+};
+
+loadData();
+</script>
