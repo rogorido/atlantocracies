80b67fadcace3688c3dfb8b5fa4d3257231997f4
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2024-05-03 13:39:50 +0200

    Component for events (table)

---
+++ components/persons/PersonsEventsTable.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <DataTable
+      :value="events"
+      paginator
+      stripedRows
+      :rows="10"
+      :rowsPerPageOptions="[5, 10, 20, 50]"
+      selectionMode="single"
+      dataKey="_id"
+      tableStyle="min-width: 50rem"
+    >
+      <Column
+        v-for="col of columns"
+        :key="col.field"
+        :field="col.field"
+        :header="col.header"
+      ></Column>
+    </DataTable>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  events: { type: Array, required: true },
+});
+
+const columns = [
+  { field: "typeEv", header: "Type" },
+  { field: "dateEv", header: "Date" },
+  { field: "eventYear", header: "Year" },
+  // { field: "histBirth", header: "Birth territory" },
+  // { field: "wasMarried", header: "Was married" },
+  //  { field: "hasFather", header: "Has father" },
+  // { field: "hasMother", header: "Has mother" },
+  // { field: "birthyear", header: "Birth year" },
+  //  { field: "tiposEventos", header: "Eventos" },
+];
+</script>
