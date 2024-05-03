e560393c7459ed521710487ce2a4bed98accb60a
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2024-05-03 13:37:50 +0200

    Small changes.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,9 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   build: {},
+  // ssr: true,
   ssr: false,
-  modules: ["nuxt-primevue", "@nuxt/content"],
+  modules: ["nuxt-primevue", "@nuxt/content", "@pinia/nuxt"],
   $development: {
     debug: true,
   },
