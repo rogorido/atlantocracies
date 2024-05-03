16e2bf0d85781905ba5059fcf496096e1496edb6
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2024-05-03 13:51:42 +0200

    Persons view: working.

--- pages/persons.vue
+++ pages/persons.vue
@@ -1,9 +1,76 @@
-<script lang="ts" setup></script>
-
 <template>
   <div>
-    Page: persons
+    <h1 class="text-center">Persons</h1>
+    <div class="grid">
+      <div class="col-6">
+        <h2>Gender</h2>
+
+        <GendersChart
+          v-if="loaded"
+          :chartData="gendersData"
+          :chartOptions="options"
+        />
+      </div>
+
+      <div class="col-6">
+        <h2>Birth places (historical)</h2>
+
+        <Chart
+          v-if="loaded"
+          type="pie"
+          :data="histbirths"
+          :options="optionshistbirths"
+        />
+      </div>
+    </div>
+    <Chart v-if="loaded" type="bar" :data="birthyears" class="w-full" />
+
+    <PersonsTable :persons="persons" v-if="loaded" />
   </div>
 </template>
 
-<style scoped></style>
+<script setup>
+import Chart from "primevue/chart";
+
+import { useStatsStore } from "../stores/statsStore";
+
+const statsstore = useStatsStore();
+
+const persons = ref();
+const histbirths = ref();
+const birthyears = ref();
+
+const loaded = ref(false);
+const gendersData = statsstore.stats.gendersChartData;
+
+const options = { responsive: true };
+
+// https://www.chartjs.org/docs/latest/configuration/legend.html
+// pies need plugins, legned, etc.
+const optionshistbirths = {
+  responsive: true,
+  plugins: { legend: { display: true, position: "right" } },
+};
+
+const loadPersonsData = async () => {
+  try {
+    const { data: cojones } = await useFetch("http://127.0.0.1:8008/persons/");
+    const { data: leches } = await useFetch(
+      "http://127.0.0.1:8008/persons/histbirths"
+    );
+    const { data: puta } = await useFetch(
+      "http://127.0.0.1:8008/persons/birthyears"
+    );
+
+    persons.value = cojones.value;
+    histbirths.value = leches.value.chartData;
+    birthyears.value = puta.value;
+
+    loaded.value = true;
+  } catch (err) {
+    console.log("error en mymap5", err);
+  }
+};
+
+loadPersonsData();
+</script>
