108e105b05b042a16e3970eedfccf33df8d55c6a
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2024-05-06 00:29:58 +0200

    using runtimeConfig in nuxt.config.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,19 @@
+const isProdEnv = process.env.NODE_ENV === "production";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
   build: {},
-  // ssr: true,
+  //ssr: true,
   ssr: false,
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: isProdEnv
+        ? process.env.API_BASE_URL
+        : "http://localhost:8008",
+    },
+  },
   modules: ["nuxt-primevue", "@nuxt/content", "@pinia/nuxt"],
   $development: {
     debug: true,
