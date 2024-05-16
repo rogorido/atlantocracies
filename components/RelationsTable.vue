df2d08470366efad8ae7faf321eb53a2e3785943
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2024-05-16 22:40:17 +0200

    Relations page

---
+++ components/RelationsTable.vue
@@ -0,0 +1,62 @@
+<template>
+  <DataTable
+    :value="relations"
+    paginator
+    stripedRows
+    :rows="10"
+    :rowsPerPageOptions="[5, 10, 20, 50]"
+    selectionMode="single"
+    dataKey="_id"
+    tableStyle="min-width: 50rem"
+  >
+    <Column
+      v-for="col of columns"
+      :key="col.field"
+      :field="col.field"
+      :header="col.header"
+    ></Column>
+  </DataTable>
+</template>
+
+<script setup>
+const props = defineProps({
+  relations: {
+    type: Array,
+    required: true,
+    default: () => [],
+  },
+});
+
+// const storepersons = usePersonsStore();
+// const toast = useToast();
+// const selectedPerson = ref();
+
+// // TODO: la cuestioón es qué hacer con el slug. Tengo una función de chatgpt
+// // pero si lo paso así luego en la página [placebyid] no puedo acceder al nombre
+// // con lo que debería meterlo en un store... o pasarlo con un parámetro, pero no
+// // veo cómo es posible... es posible con query, pero eso es otra cosa...
+// function probar() {
+//   // return navigateTo(`/place/${selectedPlace.value}`, {
+//   //   open: { target: "_blank" },
+//   // });
+
+//   return navigateTo(`/person/${selectedPerson.value}`);
+// }
+
+// const onRowSelect = (event) => {
+//   toast.add({
+//     severity: "info",
+//     summary: "Person",
+//     detail: event.data.name,
+//     life: 2000,
+//   });
+//   selectedPerson.value = event.data._id;
+//   storepersons.selectPerson(selectedPerson.value);
+//   probar();
+// };
+
+const columns = [
+  { field: "_id", header: "Relation" },
+  { field: "count", header: "Total" },
+];
+</script>
--- pages/relations.vue
+++ pages/relations.vue
@@ -1,7 +1,26 @@
 <template>
   <div>
     <h1 class="text-center">Relations</h1>
+
+    <RelationsTable :relations="relations" tabStyle="max-width: 50rem" />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRelationsStore } from "../stores/relationsStore";
+
+const relationsstore = useRelationsStore();
+
+const relations = ref([]);
+const loaded = ref(false);
+
+const config = useRuntimeConfig();
+const api = config.public.apiBaseUrl;
+
+if (!relationsstore.initialized) {
+  await relationsstore.fetchRelations();
+}
+
+relations.value = relationsstore.relations;
+loaded.value = true;
+</script>
