e6f987d83e899bbc0092b76d02733b7582122d50
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2024-05-20 10:35:11 +0200

    New file groups.vue

---
+++ pages/groups.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1 class="text-center">Analyzing groups</h1>
+    <h2 class="text-center uppercase">Persons analyzed</h2>
+    <PersonsTable :persons="groups.persons" />
+
+    <h2 class="text-center uppercase">Global insights</h2>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  groups: {
+    type: Array,
+    required: true,
+    default: () => [],
+  },
+});
+</script>
