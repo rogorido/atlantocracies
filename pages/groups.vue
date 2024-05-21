099fa66b02e5aec6d60aa2b56b63e85bd727cec0
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2024-05-21 19:03:09 +0200

    Better groups page.

--- pages/groups.vue
+++ pages/groups.vue
@@ -5,13 +5,31 @@
 
     <h2 class="text-center uppercase">Global insights</h2>
     <SearchInsights />
+    <div v-if="pending">Cargando datos...</div>
+    <GroupsRelationsGraph :personsrelated="data.personsrelationscyto" v-else />
   </div>
 </template>
 
 <script setup>
 import { usePersonsStore } from "@/stores/personsStore";
+import { useFilterStore } from "@/stores/filterStore";
+
+const config = useRuntimeConfig();
+const api = config.public.apiBaseUrl;
 
 const store = usePersonsStore();
 const { persons } = storeToRefs(store);
+
+const storefilter = useFilterStore();
+const { filter } = storeToRefs(storefilter);
+
+
+const { data, pending, error } = await useFetch(`${api}/groups`, {
+  method: "POST",
+  body:
+    filter.value,
+});
+
+console.log(data.value.personsrelationscyto);
 
 </script>
