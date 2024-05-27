8e74a56bc8d2a342440c4edac948a2d84e29bb46
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2024-05-27 16:15:57 +0200

    Prettier and no icons for null in wasMarried, etc.

--- components/Search/MacroTablePersons.vue
+++ components/Search/MacroTablePersons.vue
@@ -1,62 +1,161 @@
 <template>
   <div>
     <h2 class="mt-5 text-center uppercase">Selected Persons</h2>
-    <DataTable :value="persons" ref="dt" paginator stripedRows :rows="10" :rowsPerPageOptions="[5, 10, 20, 50]"
-      selectionMode="single" dataKey="_id" :selection="selectedPerson" @rowSelect="onRowSelect"
-      v-model:filters="filters" filterDisplay="row" tableStyle="min-width: 25rem">
+    <DataTable
+      :value="persons"
+      ref="dt"
+      paginator
+      stripedRows
+      :rows="10"
+      :rowsPerPageOptions="[5, 10, 20, 50]"
+      selectionMode="single"
+      dataKey="_id"
+      :selection="selectedPerson"
+      @rowSelect="onRowSelect"
+      v-model:filters="filters"
+      filterDisplay="row"
+      tableStyle="min-width: 25rem"
+    >
       <template #header>
         <div style="text-align: right">
-          <Button icon="pi pi-external-link" label="Export" @click="exportCSV($event)" />
+          <Button
+            icon="pi pi-external-link"
+            label="Export"
+            @click="exportCSV($event)"
+          />
         </div>
       </template>
 
       <Column field="name" header="Name" sortable>
         <template #filter="{ filterModel, filterCallback }">
-          <InputText v-model="filterModel.value" type="text" @input="filterCallback()" class="p-column-filter"
-            placeholder="Search by name" />
+          <InputText
+            v-model="filterModel.value"
+            type="text"
+            @input="filterCallback()"
+            class="p-column-filter"
+            placeholder="Search by name"
+          />
         </template>
       </Column>
-      <Column field="birthyear" header="Birth Year" dataType="numeric" style="min-width: 3rem" sortable />
-      <Column field="placebirth" header="Birth Place" dataType="string" style="min-width: 6rem" sortable />
-      <Column field="datedeath" header="Death date" dataType="string" style="min-width: 6rem" sortable />
-      <Column field="placeDeath" header="Death Place" dataType="string" style="min-width: 6rem" sortable />
-      <Column field="gender" header="Gender" dataType="boolean" style="min-width: 3rem">
+      <Column
+        field="birthyear"
+        header="Birth Year"
+        dataType="numeric"
+        style="min-width: 3rem"
+        sortable
+      />
+      <Column
+        field="placebirth"
+        header="Birth Place"
+        dataType="string"
+        style="min-width: 6rem"
+        sortable
+      />
+      <Column
+        field="datedeath"
+        header="Death date"
+        dataType="string"
+        style="min-width: 6rem"
+        sortable
+      />
+      <Column
+        field="placeDeath"
+        header="Death Place"
+        dataType="string"
+        style="min-width: 6rem"
+        sortable
+      />
+      <Column
+        field="gender"
+        header="Gender"
+        dataType="boolean"
+        style="min-width: 3rem"
+      >
         <template #body="{ data }">
-          <i class="pi" :class="{
-            'pi-mars text-green-500': data.gender === 'H',
-            'pi-venus text-green-500': data.gender === 'M',
-          }"></i>
+          <i
+            class="pi"
+            :class="{
+              'pi-mars text-green-500': data.gender === 'H',
+              'pi-venus text-green-500': data.gender === 'M',
+            }"
+          ></i>
         </template>
       </Column>
 
-      <Column field="wasMarried" header="Married" dataType="boolean" style="min-width: 3rem">
+      <Column
+        field="wasMarried"
+        header="Married"
+        dataType="boolean"
+        style="min-width: 3rem"
+      >
         <template #body="{ data }">
-          <i class="pi" :class="{
-            'pi-check-circle text-green-500': data.wasMarried,
-            'pi-times-circle text-red-400': !data.wasMarried,
-          }"></i>
+          <i
+            class="pi"
+            :class="{
+              'pi-check-circle text-green-500': data.wasMarried,
+            }"
+          ></i>
         </template>
       </Column>
-      <Column field="hasFather" header="Has Father" dataType="boolean" style="min-width: 6rem">
+      <Column
+        field="hasFather"
+        header="Has Father"
+        dataType="boolean"
+        style="min-width: 6rem"
+      >
         <template #body="{ data }">
-          <i class="pi" :class="{
-            'pi-check-circle text-green-500': data.hasFather,
-            'pi-times-circle text-red-400': !data.hasFather,
-          }"></i>
+          <i
+            class="pi"
+            :class="{
+              'pi-check-circle text-green-500': data.hasFather,
+              'pi-times-circle text-red-400': !data.hasFather,
+            }"
+          ></i>
         </template>
       </Column>
-      <Column field="hizoTestamento" header="Made will" dataType="boolean" style="min-width: 3rem">
+      <Column
+        field="hizoTestamento"
+        header="Made will"
+        dataType="boolean"
+        style="min-width: 3rem"
+      >
         <template #body="{ data }">
-          <i class="pi" :class="{
-            'pi-check-circle text-green-500': data.hizoTestamento,
-            'pi-times-circle text-red-400': !data.hizoTestamento,
-          }"></i>
+          <i
+            class="pi"
+            :class="{
+              'pi-check-circle text-green-500': data.hizoTestamento,
+            }"
+          ></i>
         </template>
       </Column>
-      <Column field="numberOfEvents" header="# Events" dataType="numeric" style="min-width: 3rem" sortable />
-      <Column field="numberofRelations" header="# Relations" dataType="numeric" style="min-width: 3rem" sortable />
-      <Column field="numberofPositions" header="# Positions" dataType="numeric" style="min-width: 3rem" sortable />
-      <Column field="numberofTitles" header="# Titles" dataType="numeric" style="min-width: 3rem" sortable />
+      <Column
+        field="numberOfEvents"
+        header="# Events"
+        dataType="numeric"
+        style="min-width: 3rem"
+        sortable
+      />
+      <Column
+        field="numberofRelations"
+        header="# Relations"
+        dataType="numeric"
+        style="min-width: 3rem"
+        sortable
+      />
+      <Column
+        field="numberofPositions"
+        header="# Positions"
+        dataType="numeric"
+        style="min-width: 3rem"
+        sortable
+      />
+      <Column
+        field="numberofTitles"
+        header="# Titles"
+        dataType="numeric"
+        style="min-width: 3rem"
+        sortable
+      />
     </DataTable>
   </div>
 </template>
