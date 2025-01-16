ef6dde2b02fd860c72ab4c4e7abe16977785a6c7
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2025-01-16 23:39:22 +0100

    Filter titles by year ranges.

--- components/Search/TitlesSelect.vue
+++ components/Search/TitlesSelect.vue
@@ -10,18 +10,53 @@
     :maxSelectedLabels="3"
     class="mb-3 w-full"
   />
-  <MultiSelect
-    v-model="selectedContinents"
-    display="chip"
-    v-show="showContinent"
-    filter
-    :loading="loading"
-    :options="titlescontinents"
-    optionLabel="_id"
-    placeholder="Select continent"
-    :maxSelectedLabels="3"
-    class="mb-3 w-full"
-  />
+  <!-- We have to use v-if and not v-show because if v-show is only for elements.  -->
+  <div v-if="checked" class="gap-3 mb-3">
+    <MultiSelect
+      v-model="selectedContinents"
+      display="chip"
+      filter
+      :loading="loading"
+      :options="titlescontinents"
+      optionLabel="_id"
+      placeholder="Select continent"
+      :maxSelectedLabels="3"
+      class="mb-3 w-full"
+    />
+  </div>
+  <div v-if="checked" class="flex gap-3 mb-3">
+    <p>Event happened between</p>
+    <InputNumber
+      v-model="selectedYears[0]"
+      showButtons
+      buttonLayout="horizontal"
+      :min="1400"
+      :max="1900"
+    >
+      <template #incrementbuttonicon>
+        <span class="pi pi-plus" />
+      </template>
+      <template #decrementbuttonicon>
+        <span class="pi pi-minus" />
+      </template>
+    </InputNumber>
+    <p>and</p>
+    <InputNumber
+      v-model="selectedYears[1]"
+      showButtons
+      buttonLayout="horizontal"
+      :min="1400"
+      :max="1900"
+    >
+      <template #incrementbuttonicon>
+        <span class="pi pi-plus" />
+      </template>
+      <template #decrementbuttonicon>
+        <span class="pi pi-minus" />
+      </template>
+    </InputNumber>
+    <Button label="Reset dates" @click="resetYears" />
+  </div>
 </template>
 
 <script setup>
@@ -37,7 +72,8 @@ if (!storetitles.initialized === true) {
   await storetitles.fetchTitles();
 }
 
-const showContinent = ref(false);
+const checked = ref(false);
+const selectedYears = ref([1400, 1900]);
 
 items.value = storetitles.titlesList;
 const titlescontinents = storetitles.titlesContinentsList;
@@ -48,7 +84,8 @@ watch(selectedItems, () => {
     if (selectedItems.value.length === 0) {
       delete filter.value.titles;
       selectedContinents.value = [];
-      showContinent.value = false;
+      // no reinicializo seelectedYears porque me da un error tal y como está ahora...
+      checked.value = false;
     } else {
       // Initialize filter.titles if it doesn't exist
       filter.value.titles = filter.value.titles || {};
@@ -61,7 +98,12 @@ watch(selectedItems, () => {
         (title) => title._id,
       );
 
-      showContinent.value = true;
+      // esto realmente hace falta aquí?
+      if (selectedYears.value[0] != 1400 && selectedYears.value[1] != 1900) {
+        filter.value.titles.yearsrange = selectedYears.value;
+      }
+
+      checked.value = true;
     }
   }
 });
@@ -87,6 +129,21 @@ watch(selectedContinents, () => {
   }
 });
 
+watch(
+  selectedYears,
+  () => {
+    if (selectedYears.value[0] != 1400 && selectedYears.value[1] != 1900) {
+      filter.value.titles.yearsrange = selectedYears.value;
+    } else if (
+      selectedYears.value[0] === 1400 &&
+      selectedYears.value[1] === 1900
+    ) {
+      delete filter.value.titles.yearsrange;
+    }
+  },
+  { deep: true },
+);
+//
 // TODO: habría que cargar tb el valor de los otros campos (continentes, etc.)
 onMounted(() => {
   // IMPORTANT: we have to add "&& filter.value.titles" because otherwise
@@ -101,4 +158,8 @@ onMounted(() => {
     }
   }
 });
+
+function resetYears() {
+  selectedYears.value = [1400, 1900];
+}
 </script>
