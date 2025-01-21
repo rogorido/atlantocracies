f374b311fef670e243ce23dde34d084775d4c1e9
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2025-01-21 19:55:26 +0100

    New component placestable for eventbyid.

---
+++ components/Events/PlacesTable.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <DataTable
+      :value="places"
+      paginator
+      stripedRows
+      :rows="10"
+      :rowsPerPageOptions="[5, 10, 20, 50]"
+      selectionMode="single"
+      dataKey="_id"
+      tableStyle="min-width: 50rem"
+    >
+      <template #empty> No places found. </template>
+      <Column
+        v-for="col of columns"
+        :key="col.field"
+        :field="col.field"
+        :header="col.header"
+      ></Column>
+    </DataTable>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  places: { type: Array, required: true, default: () => [] },
+});
+
+const columns = [
+  { field: "_id", header: "Place" },
+  { field: "count", header: "Total" },
+  { field: "percent", header: "%" },
+];
+</script>
