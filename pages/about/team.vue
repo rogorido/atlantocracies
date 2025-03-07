98a1b5a2fceeef8dcaf57dba04705544140a18cf
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2025-03-07 19:32:24 +0100

    Team page.

--- pages/about/team.vue
+++ pages/about/team.vue
@@ -1,5 +1,496 @@
 <template>
-  <div>
-    <h1 class="text-center">About</h1>
+  <div class="p-container">
+    <h1 class="text-center">About our team</h1>
+
+    <h2>Bartolomé Yun-Casalilla</h2>
+    <p>
+      <a href="https://www.upo.es/profesorado/byuncas/"
+        ><img
+          decoding="async"
+          src="https://atlantocracies.com/wp-content/uploads/2023/11/BYC.png"
+          alt=""
+          width="120"
+          height="168"
+          class="contacto-imagen alignleft wp-image-472"
+      /></a>
+    </p>
+    <h4>Investigador Principal</h4>
+    <p class="card-description" style="text-align: justify">
+      <span style="font-size: 14px; color: #000000"
+        >Catedrático de historia moderna en la Universidad Pablo de Olavide,
+        Sevilla, España. Está especializado en el estudio de la economía
+        política, el Imperio español, la historia del consumo y la historia de
+        la aristocracia.</span
+      >
+    </p>
+    <p>
+      <a href="https://orcid.org/0000-0002-8095-5733"
+        ><img
+          decoding="async"
+          src="https://i0.wp.com/bibliosjd.org/wp-content/uploads/2018/09/id_orcid.png?fit=256%2C258&amp;ssl=1"
+          alt="ORCID logo"
+          width="32"
+          height="32"
+      /></a>
+      <a
+        style="color: #000000; text-decoration: underline"
+        href="https://www.webofscience.com/wos/author/record/AAS-8123-2021"
+        ><img
+          decoding="async"
+          src="https://datai.infor.uva.es/wp-content/uploads/2018/04/researcher-id-logo.png"
+          alt="ResearchID"
+          width="36"
+          height="36" /></a
+      ><span style="font-size: 12px">
+        <a
+          style="color: #000000; text-decoration: underline"
+          href="https://www.scopus.com/authid/detail.uri?authorId=37062330800"
+          ><img
+            loading="lazy"
+            decoding="async"
+            src="https://bib.us.es/ingenieros/sites/bib3.us.es.ingenieros/files/styles/medium/adaptive-image/public/field/image/scopus.png?itok=7a5ObggF"
+            alt="Scopus"
+            width="32"
+            height="32"
+        /></a> </span
+      ><a
+        style="color: #000000; text-decoration: underline"
+        href="https://dialnet.unirioja.es/servlet/autor?codigo=9417"
+        ><img
+          loading="lazy"
+          decoding="async"
+          src="https://desk.zoho.eu/portal/api/kbCategory/54059000000110207/logo/1576492000000006002?orgId=20073724420"
+          alt="Scopus"
+          width="32"
+          height="32"
+      /></a>
+    </p>
+    <hr />
+    <p>&nbsp;</p>
+    <h2>Manuel Díaz-Ordóñez</h2>
+    <p>
+      <a
+        href="https://www.us.es/trabaja-en-la-us/directorio/manuel-diaz-ordonez"
+        ><img
+          loading="lazy"
+          decoding="async"
+          src="https://atlantocracies.com/wp-content/uploads/2023/11/1699983633278.jpg"
+          alt=""
+          width="120"
+          height="151"
+          class="contacto-imagen alignleft wp-image-476"
+      /></a>
+    </p>
+    <h4>Investigador</h4>
+    <p class="card-description" style="text-align: justify">
+      <span style="font-size: 14px; color: #000000"
+        >Profesor Titular en el Departamento de Economía e Historia Económica de
+        la Universidad de Sevilla, España. Se especializa en el estudio de
+        industrias estratégicas de aparejos navales, el Imperio español y la
+        expansión global del cáñamo.</span
+      >
+    </p>
+    <p>
+      <a href="https://orcid.org/0000-0002-3492-6210"
+        ><img
+          decoding="async"
+          src="https://i0.wp.com/bibliosjd.org/wp-content/uploads/2018/09/id_orcid.png?fit=256%2C258&amp;ssl=1"
+          alt="ORCID logo"
+          width="32"
+          height="32"
+      /></a>
+      <a
+        style="color: #000000; text-decoration: underline"
+        href="https://www.webofscience.com/wos/author/record/A-5215-2015"
+        ><img
+          decoding="async"
+          src="https://datai.infor.uva.es/wp-content/uploads/2018/04/researcher-id-logo.png"
+          alt="ResearchID"
+          width="36"
+          height="36" /></a
+      ><span style="font-size: 12px">
+        <a
+          style="color: #000000; text-decoration: underline"
+          href="https://www.scopus.com/authid/detail.uri?authorId=57205248423"
+          ><img
+            loading="lazy"
+            decoding="async"
+            src="https://bib.us.es/ingenieros/sites/bib3.us.es.ingenieros/files/styles/medium/adaptive-image/public/field/image/scopus.png?itok=7a5ObggF"
+            alt="Scopus"
+            width="32"
+            height="32"
+        /></a> </span
+      ><a
+        style="color: #000000; text-decoration: underline"
+        href="https://dialnet.unirioja.es/servlet/autor?codigo=298758"
+        ><img
+          loading="lazy"
+          decoding="async"
+          src="https://desk.zoho.eu/portal/api/kbCategory/54059000000110207/logo/1576492000000006002?orgId=20073724420"
+          alt="Scopus"
+          width="32"
+          height="32"
+      /></a>
+    </p>
+    <hr />
+    <p>&nbsp;</p>
+    <h2>Domingo Savio Rodríguez-Baena</h2>
+    <p>
+      <a href="https://www.upo.es/profesorado/dsrodbae"
+        ><img
+          loading="lazy"
+          decoding="async"
+          src="https://atlantocracies.com/wp-content/uploads/2023/11/Domingo.jpg"
+          alt=""
+          width="122"
+          height="156"
+          class="contacto-imagen alignleft wp-image-478"
+      /></a>
+    </p>
+    <h4>Investigador</h4>
+    <p class="card-description" style="text-align: justify">
+      <span style="font-size: 14px; color: #000000"
+        >Profesor titular de la Universidad Pablo de Olavide, España. Está
+        especializado en el análisis inteligente de datos mediante técnicas de
+        Machine Learning y minería de datos además de experto en diseño y
+        gestión de bases de datos.</span
+      >
+    </p>
+    <p>
+      <a href="https://orcid.org/0000-0001-9801-9364"
+        ><img
+          decoding="async"
+          src="https://i0.wp.com/bibliosjd.org/wp-content/uploads/2018/09/id_orcid.png?fit=256%2C258&amp;ssl=1"
+          alt="ORCID logo"
+          width="32"
+          height="32"
+      /></a>
+      <a
+        style="color: #000000; text-decoration: underline"
+        href="https://www.webofscience.com/wos/author/record/L-4715-2014"
+        ><img
+          decoding="async"
+          src="https://datai.infor.uva.es/wp-content/uploads/2018/04/researcher-id-logo.png"
+          alt="ResearchID"
+          width="36"
+          height="36" /></a
+      ><span style="font-size: 12px">
+        <a
+          style="color: #000000; text-decoration: underline"
+          href="https://www.scopus.com/authid/detail.uri?authorId=6506478276"
+          ><img
+            loading="lazy"
+            decoding="async"
+            src="https://bib.us.es/ingenieros/sites/bib3.us.es.ingenieros/files/styles/medium/adaptive-image/public/field/image/scopus.png?itok=7a5ObggF"
+            alt="Scopus"
+            width="32"
+            height="32"
+        /></a> </span
+      ><a
+        style="color: #000000; text-decoration: underline"
+        href="https://dialnet.unirioja.es/servlet/autor?codigo=793167"
+        ><img
+          loading="lazy"
+          decoding="async"
+          src="https://desk.zoho.eu/portal/api/kbCategory/54059000000110207/logo/1576492000000006002?orgId=20073724420"
+          alt="Scopus"
+          width="32"
+          height="32"
+      /></a>
+    </p>
+    <hr />
+    <p>&nbsp;</p>
+    <h2>Alejandro García-Montón</h2>
+    <p>
+      <a
+        href="https://laborales.ugr.es/facultad/directorio-personal/alejandro-garcia-monton"
+        ><img
+          loading="lazy"
+          decoding="async"
+          src="https://atlantocracies.com/wp-content/uploads/2023/11/alejandroupo_web-1.jpg"
+          alt=""
+          width="120"
+          height="160"
+          class="contacto-imagen alignleft wp-image-519 size-full"
+      /></a>
+    </p>
+    <h4>Investigador</h4>
+    <p class="card-description" style="text-align: justify">
+      <span style="font-size: 14px; color: #000000"
+        >Profesor ayudante de doctor en la Universidad de Granada. Sus
+        principales intereses se centran en las redes mercantiles de la edad
+        moderna y el comercio trans-imperial de esclavos en el mundo
+        atlántico.</span
+      ><a href="https://orcid.org/0000-0003-0311-1876"
+        ><br />
+        <img
+          decoding="async"
+          src="https://i0.wp.com/bibliosjd.org/wp-content/uploads/2018/09/id_orcid.png?fit=256%2C258&amp;ssl=1"
+          alt="ORCID logo"
+          width="32"
+          height="32"
+      /></a>
+      <span style="font-size: 12px">
+        <a
+          style="color: #000000; text-decoration: underline"
+          href="https://www.scopus.com/authid/detail.uri?authorId=57210579464"
+          ><img
+            loading="lazy"
+            decoding="async"
+            src="https://bib.us.es/ingenieros/sites/bib3.us.es.ingenieros/files/styles/medium/adaptive-image/public/field/image/scopus.png?itok=7a5ObggF"
+            alt="Scopus"
+            width="32"
+            height="32"
+        /></a> </span
+      ><a
+        style="color: #000000; text-decoration: underline"
+        href="https://dialnet.unirioja.es/servlet/autor?codigo=3319545"
+        ><img
+          loading="lazy"
+          decoding="async"
+          src="https://desk.zoho.eu/portal/api/kbCategory/54059000000110207/logo/1576492000000006002?orgId=20073724420"
+          alt="Scopus"
+          width="32"
+          height="32"
+      /></a>
+    </p>
+    <hr />
+    <h1 style="text-decoration: underline; text-align: center">
+      Equipo de colaboradores
+    </h1>
+    <hr />
+    <p>&nbsp;</p>
+    <h2>Jean-Pierre Dedieu</h2>
+    <p>
+      <img
+        loading="lazy"
+        decoding="async"
+        src="https://atlantocracies.com/wp-content/uploads/2023/11/Photo_JPDedieu.jpg"
+        alt=""
+        width="133"
+        height="160"
+        class="contacto-imagen alignleft wp-image-651"
+      />
+    </p>
+    <h4>Colaborador</h4>
+    <p class="card-description" style="text-align: justify">
+      <span style="font-size: 14px; color: #000000"
+        >Historiador francés, especializado en la historia moderna de España e
+        Hispanoamérica, sus primeras obras están enfocadas al estudio de la la
+        inquisición española. Creador y gestor de la Base de Datos Fichoz.
+        Discípulo del hispanista e historiador Bartolomé Bennassar.
+      </span>
+    </p>
+    <p class="card-description" style="text-align: justify">
+      <span style="font-size: 14px; color: #000000"></span
+      ><a href="https://orcid.org/0000-0001-5727-2427"
+        ><span style="color: #000000"></span></a
+      ><a href="https://orcid.org/0000-0002-5684-4718"
+        ><img
+          loading="lazy"
+          decoding="async"
+          src="https://i0.wp.com/bibliosjd.org/wp-content/uploads/2018/09/id_orcid.png?fit=256%2C258&amp;ssl=1"
+          alt="ORCID logo"
+          width="32"
+          height="32"
+          class="alignnone" /></a
+      ><span style="color: #000000"><span style="font-size: 12px"> </span></span
+      ><a
+        style="color: #000000; text-decoration: underline"
+        href="https://dialnet.unirioja.es/servlet/autor?codigo=66116"
+        ><img
+          loading="lazy"
+          decoding="async"
+          src="https://desk.zoho.eu/portal/api/kbCategory/54059000000110207/logo/1576492000000006002?orgId=20073724420"
+          alt="Scopus"
+          width="32"
+          height="32"
+          class="alignnone"
+      /></a>
+    </p>
+    <hr />
+    <p>&nbsp;</p>
+    <h2>Jorge Rojas-Gutiérrez</h2>
+    <p>
+      <a href="https://www.upo.es/investiga/globalhistory/jorge-rojas/"
+        ><img
+          loading="lazy"
+          decoding="async"
+          src="https://atlantocracies.com/wp-content/uploads/2023/11/Jorge-Rojas-Foto-2-236x300-1.jpeg"
+          alt=""
+          width="120"
+          height="153"
+          class="contacto-imagen alignleft wp-image-522"
+      /></a>
+    </p>
+    <h4>Colaborador</h4>
+    <p class="card-description" style="text-align: justify">
+      <span style="font-size: 14px; color: #000000">
+        Graduado en Historia por la Universidad de Castilla-La Mancha, cursó el
+        Máster en Historia y Humanidades Digitales en la Universidad Pablo de
+        Olavide de Sevilla, donde actualmente es estudiante de doctorado en el
+        marco de una ayuda FPU. Su área de investigación aborda la circulación
+        de las élites a través de distintos territorios de la Monarquía
+        Hispánica.</span
+      >
+    </p>
+    <p>&nbsp;</p>
+    <hr />
+    <h2>Pedro Omar Svriz-Wucherer</h2>
+    <p>
+      <a href="https://investigacion.us.es/sisius/sis_showpub.php?idpers=31871"
+        ><img
+          loading="lazy"
+          decoding="async"
+          src="https://atlantocracies.com/wp-content/uploads/2023/11/Pedro-Miguel-Omar-Svriz-Wucherer.jpg"
+          alt=""
+          width="122"
+          height="155"
+          class="contacto-imagen alignleft wp-image-108"
+      /></a>
+    </p>
+    <h4>Colaborador (hasta febrero 2021)</h4>
+    <p class="card-description" style="text-align: justify">
+      <span style="font-size: 14px; color: #000000"
+        >Doctor en Historia y Humanidades por la UPO. Su tesis doctoral versó
+        sobre las milicias guaraníes de las Reducciones Jesuíticas de Paraguay
+        entre los siglos XVII y XVIII. Se preocupa principalmente por cómo las
+        monarquías y las instituciones jesuitas y las variables económicas,
+        sociales y culturales se combinaron para defender la frontera del
+        imperio español.</span
+      >
+    </p>
+    <p class="card-description" style="text-align: justify">
+      <span style="font-size: 14px; color: #000000"></span
+      ><a href="https://orcid.org/0000-0001-5727-2427"
+        ><span style="color: #000000"></span></a
+      ><a href="https://orcid.org/0000-0001-8313-3009"
+        ><img
+          decoding="async"
+          src="https://i0.wp.com/bibliosjd.org/wp-content/uploads/2018/09/id_orcid.png?fit=256%2C258&amp;ssl=1"
+          alt="ORCID logo"
+          width="32"
+          height="32" /></a
+      ><span style="color: #000000">
+        <span style="font-size: 12px">
+          <a
+            style="color: #000000; text-decoration: underline"
+            href="https://www.scopus.com/authid/detail.uri?authorId=57202493740"
+            ><img
+              loading="lazy"
+              decoding="async"
+              src="https://bib.us.es/ingenieros/sites/bib3.us.es.ingenieros/files/styles/medium/adaptive-image/public/field/image/scopus.png?itok=7a5ObggF"
+              alt="Scopus"
+              width="32"
+              height="32"
+          /></a> </span></span
+      ><a
+        style="color: #000000; text-decoration: underline"
+        href="https://dialnet.unirioja.es/servlet/autor?codigo=3027610"
+        ><img
+          loading="lazy"
+          decoding="async"
+          src="https://desk.zoho.eu/portal/api/kbCategory/54059000000110207/logo/1576492000000006002?orgId=20073724420"
+          alt="Scopus"
+          width="32"
+          height="32"
+      /></a>
+    </p>
+    <hr />
+    <h1 style="text-align: center; text-decoration: underline">
+      Equipo de asistentes
+    </h1>
+    <hr />
+    <p>&nbsp;</p>
+    <h2>María Jesús Milán-Agudo</h2>
+    <p>
+      <img
+        loading="lazy"
+        decoding="async"
+        src="https://atlantocracies.com/wp-content/uploads/2023/11/Maria-Jesus-Milan.jpg"
+        alt=""
+        width="118"
+        height="150"
+        class="contacto-imagen alignleft wp-image-104 size-thumbnail"
+      />
+    </p>
+    <h4>Asistente de Investigación</h4>
+    <p class="card-description" style="text-align: justify">
+      <span style="font-size: 14px; color: #000000"
+        >Licenciada en Geografía e Historia por la Universidad de Barcelona.
+        Anteriormente, desarrolló su labor como Titulado Superior de Apoyo a la
+        Investigación en la Universidad Pablo de Olavide de Sevilla. Es coautora
+        de varios libros y artículos sobre historia moderna y contemporánea de
+        España.</span
+      >
+    </p>
+    <p>
+      <a href="https://orcid.org/0000-0003-2032-9930"
+        ><img
+          decoding="async"
+          src="https://i0.wp.com/bibliosjd.org/wp-content/uploads/2018/09/id_orcid.png?fit=256%2C258&amp;ssl=1"
+          alt="ORCID logo"
+          width="32"
+          height="32" /></a
+      ><a
+        style="color: #000000; text-decoration: underline"
+        href="https://dialnet.unirioja.es/servlet/autor?codigo=4820191"
+        ><img
+          loading="lazy"
+          decoding="async"
+          src="https://desk.zoho.eu/portal/api/kbCategory/54059000000110207/logo/1576492000000006002?orgId=20073724420"
+          alt="Scopus"
+          width="32"
+          height="32"
+      /></a>
+    </p>
+    <hr />
+    <p>
+      <a
+        style="color: #000000; text-decoration: underline"
+        href="https://dialnet.unirioja.es/servlet/autor?codigo=4820191"
+      ></a>
+    </p>
+    <h1 style="text-align: center; text-decoration: underline">
+      Equipo de diseñadores web
+    </h1>
+    <hr />
+    <h2>Adrián Díaz-Milán</h2>
+    <p>
+      <img
+        loading="lazy"
+        decoding="async"
+        src="https://atlantocracies.com/wp-content/uploads/2023/11/IMG_20230629_155734-scaled.jpg"
+        alt=""
+        width="128"
+        height="135"
+        class="contacto-imagen alignleft wp-image-780"
+      />
+    </p>
+    <h4>Diseñador web</h4>
+    <p class="card-description" style="text-align: justify">
+      <span style="font-size: 14px; color: #000000"
+        >Graduado en Geografía e Historia por la Universidad Pablo de Olavide,
+        Sevilla, España. Su área general de interés es el análisis de la
+        movilización transfronteriza de las élites europeas y la aplicación de
+        nuevas tecnologías digitales como uso metodológico en la investigación
+        histórica.</span
+      >
+    </p>
+    <p>
+      <a href="https://orcid.org/0000-0001-9638-4130"
+        ><img
+          loading="lazy"
+          decoding="async"
+          src="https://i0.wp.com/bibliosjd.org/wp-content/uploads/2018/09/id_orcid.png?fit=256%2C258&amp;ssl=1"
+          alt="ORCID logo"
+          width="32"
+          height="32"
+          class="alignnone"
+      /></a>
+    </p>
+    <hr />
+    <p><a href="https://orcid.org/0000-0001-9638-4130"></a></p>
+    <hr />
   </div>
 </template>
