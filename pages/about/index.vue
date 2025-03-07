fe7484493cf7909e3a3da4260c00357a478d1ef5
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2025-03-07 19:22:50 +0100

    Reordering about pages.

---
+++ pages/about/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="p-container">
+    <h1 class="text-center">About</h1>
+
+    <p>
+      You can know more about the <NuxtLink to="/about/team">Team</NuxtLink> and
+      about the <NuxtLink to="/about/software">software</NuxtLink> used in this
+      page.
+    </p>
+  </div>
+</template>
---
+++ pages/about/software.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="p-container">
+    <h1 class="text-center">About the sofware</h1>
+
+    <h2 class="text-center">Our software</h2>
+
+    <p>The code used for this page has two aspects:</p>
+    <ul>
+      <li>
+        Application Programming Interface (API) which communicates with the
+        MongoDB where the data are stored. The API is written in javascript and
+        is run under <a href="https://nodejs.org/">Nodejs</a>. The code is
+        released under the
+        <a href="https://www.gnu.org/licenses/agpl-3.0.html"
+          >GNU Affero General Public License</a
+        >
+        (version 3.0) and is accesible
+        <a href="https://github.com/rogorido/apiatlantocracies">here</a>.
+      </li>
+      <li>
+        This website is programmed using the framework
+        <a href="https://nuxt.com">Nuxtjs</a>, which is based on
+        <a href="https://vuejs.com">Vuejs</a>. The code is released under the
+        <a href="https://www.gnu.org/licenses/gpl-3.0.html"
+          >GNU General Public License</a
+        >
+        (version 3.0) and is accesible
+        <a href="https://github.com/rogorido/atlantocracies">here</a>.
+      </li>
+    </ul>
+
+    <h2 class="text-center">Sofware used</h2>
+
+    <p>In the present page we have used the following software:</p>
+    <ul>
+      <li>
+        As already noted, this website is programmed using the framework
+        <a href="https://nuxt.com">Nuxtjs</a>, which is based on
+        <a href="https://vuejs.com">Vuejs</a>.
+      </li>
+      <li>
+        We use the library <a href="https://www.chartjs.org/">Chartjs</a> for
+        our charts.
+      </li>
+      <li>
+        We use the library <a href="https://openlayers.org/">Openlayers</a> for
+        our maps.
+      </li>
+      <li>
+        We use the library <a href="https://cytoscape.org/">Cytoscape</a> for
+        visualizing networks and the
+        <a href="https://github.com/iVis-at-Bilkent/cytoscape.js-fcose"
+          >fcose algorithm</a
+        >.
+      </li>
+      <li>
+        We use the library <a href="https://primevue.org/">Primevue</a> for UI
+        components.
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+li {
+  max-width: 72ch;
+}
+</style>
