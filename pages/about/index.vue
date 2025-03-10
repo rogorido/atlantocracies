9298a2df24279d5f3dc8cb5005a0a4aaa80bd34a
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2025-03-10 18:54:14 +0100

    Better about page.

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -2,10 +2,88 @@
   <div class="p-container">
     <h1 class="text-center">About</h1>
 
-    <p>
-      You can know more about the <NuxtLink to="/about/team">Team</NuxtLink> and
-      about the <NuxtLink to="/about/software">software</NuxtLink> used in this
-      page.
-    </p>
+    <section>
+      <div class="grid oferta" @click="goToSite('/about/team')">
+        <div class="col-12 md:col-6 imageshown">
+          <img
+            src="@/assets/svg/pattern_03.svg"
+            alt="houses"
+            style="border-radius: 10% 30% 50% 70%"
+          />
+        </div>
+        <div class="col-12 md:col-6 descriptionshown">
+          <h3>Our Team</h3>
+          <p>
+            You can know more about the
+            <NuxtLink to="/about/team">Team</NuxtLink> of Atlantocracies.
+          </p>
+        </div>
+      </div>
+
+      <div class="grid oferta" @click="goToSite('/about/software')">
+        <div class="col-12 md:col-6 imageshown">
+          <img
+            src="@/assets/svg/pattern_04.svg"
+            alt="houses"
+            style="border-radius: 10% 30% 50% 70%"
+          />
+        </div>
+        <div class="col-12 md:col-6 descriptionshown">
+          <h3>Software used</h3>
+          <p>
+            You can know more about the
+            <NuxtLink to="/about/software">software</NuxtLink> used in this
+            page.
+          </p>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
+
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const goToSite = (site) => {
+  router.push(site);
+};
+</script>
+
+<style lang="scss" scoped>
+img {
+  height: 300px;
+}
+
+@media (min-width: 701px) {
+  .oferta:nth-of-type(2n) {
+    .imageshown {
+      order: 2;
+    }
+
+    .descriptionshown {
+      order: 1;
+    }
+  }
+}
+
+@media (max-width: 700px) {
+  .oferta {
+    flex-direction: column;
+
+    .imageshown {
+      order: 1;
+    }
+
+    .descriptionshown {
+      order: 2;
+      padding-top: 20px;
+    }
+  }
+
+  .q-img {
+    height: 300px;
+  }
+}
+</style>
